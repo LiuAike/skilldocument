af165b37d361c142edc1b50a84597dae42a8c2e5
LiuAiKe <lyflycq@163.com>
2020-01-21 13:55:03 +0800

    note# 初始化项目

---
+++ src/main/java/com/github/skilldocument/SkilldocumentApplication.java
@@ -0,0 +1,13 @@
+package com.github.skilldocument;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkilldocumentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkilldocumentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/skilldocument/SkilldocumentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.skilldocument;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkilldocumentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
